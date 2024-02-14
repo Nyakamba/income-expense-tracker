3804a43ec580bc25feca8abc560df5c9fc9b1a23
Nyakamba <omwegaenock@gmail.com>
2024-02-14 20:05:12 +0300

    user registration functionality

--- server/controllers/users/usersCtrl.js
+++ server/controllers/users/usersCtrl.js
@@ -1,7 +1,32 @@
+const bcrypt = require("bcryptjs");
+const User = require("../../model/User");
+
 //register
 const registerUserCtrl = async (req, res) => {
+  const { fullname, password, email } = req.body;
   try {
-    res.json({ msg: "Register route" });
+    //check is user exists
+    const userFound = await User.findOne({ email });
+    if (userFound) return res.json({ message: "User Already Exist" });
+
+    //check if fields are empty
+    if (!email || !password || !fullname)
+      return res.json({ message: "Pleas provide all fields" });
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //create user
+    const user = await User.create({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+    res.json({
+      status: "success",
+      fullname: user.fullname,
+      email: user.email,
+      id: user._id,
+    });
   } catch (error) {
     res.json(error);
   }
