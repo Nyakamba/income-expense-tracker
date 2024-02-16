117956b0260e4b939b9bd36c5899b5350c26f01d
Nyakamba <omwegaenock@gmail.com>
2024-02-16 16:19:53 +0300

    context api

---
+++ client/src/components/context/AuthContext/AuthContext.js
@@ -0,0 +1,31 @@
+const { createContext, useReducer } = require("react");
+
+//auth context
+export const authContext = createContext();
+
+//initial state
+const INITIAL_STATE = {
+  userAuth: null,
+  error: null,
+  loading: false,
+  profile: null,
+};
+
+//Auth reducer
+
+const reducer = (state, action) => {
+  return {};
+};
+
+//provider
+
+const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  return (
+    <authContext.Provider value={{ isLogin: false }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
