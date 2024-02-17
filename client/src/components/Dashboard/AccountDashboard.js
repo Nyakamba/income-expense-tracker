18341a9631233deea935d14f92bf2bda3c9e934e
Nyakamba <omwegaenock@gmail.com>
2024-02-17 17:40:07 +0300

    fetching account from backend

--- client/src/components/Dashboard/AccountDashboard.js
+++ client/src/components/Dashboard/AccountDashboard.js
@@ -26,7 +26,7 @@ const AccountDashboard = () => {
       ) : (
         <>
           <AccountSummary />
-          <AccountList />
+          <AccountList accounts={profile?.accounts} />
         </>
       )}
     </>
