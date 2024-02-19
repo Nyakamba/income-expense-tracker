35a43de5b9168dcdabd327207489e4b8f7090733
Nyakamba <omwegaenock@gmail.com>
2024-02-19 16:33:16 +0300

    adding transactions functionality

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,15 @@ import App from "./App";
 
 import AuthContextProvider from "./components/context/AuthContext/AuthContext";
 import AccountContextProvider from "./components/context/AccountContext/AccountContext";
+import { TransactionContextProvider } from "./components/context/TransactionContext/TransactionContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
     <AccountContextProvider>
-      <App />
+      <TransactionContextProvider>
+        <App />
+      </TransactionContextProvider>
     </AccountContextProvider>
   </AuthContextProvider>
 );
